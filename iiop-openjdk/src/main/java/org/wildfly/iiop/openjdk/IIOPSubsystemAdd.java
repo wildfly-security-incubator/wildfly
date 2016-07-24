74f124fe9b938ca8dbfc4a05d8a9611d7498472d
Tomasz Adamski <tadamski@redhat.com>
2016-07-24 15:51:05 +0200

    WFLY-6875 Add SSL protocol configuration to iiop-openjdk subsystem

--- iiop-openjdk/src/main/java/org/wildfly/iiop/openjdk/IIOPSubsystemAdd.java
+++ iiop-openjdk/src/main/java/org/wildfly/iiop/openjdk/IIOPSubsystemAdd.java
@@ -339,6 +339,10 @@ public class IIOPSubsystemAdd extends AbstractAddStepHandler {
             final String securityDomain = props.getProperty(Constants.SECURITY_SECURITY_DOMAIN);
             // add the domain socket factories.
             SSLSocketFactory.setSecurityDomain(securityDomain);
+
+            String enabledProtocols = props.getProperty(Constants.SECURITY_SSL_ENABLED_PROTOCOLS);
+            SSLSocketFactory.setEnabledProtocols(enabledProtocols.split(","));
+
             props.setProperty(ORBConstants.SOCKET_FACTORY_CLASS_PROPERTY, SSLSocketFactory.class.getName());
 
             sslConfigured = true;
